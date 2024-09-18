540be0386358f3b8055078ecaa56ad1608e1349b
AustinO97 <austinoblow97@gmail.com>
2024-09-18 14:36:40 -0400

    form submit working, received email

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,5 +1,6 @@
 "use client";
-import { useState } from "react";
+
+import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
@@ -12,10 +13,12 @@ import {
   SelectValue,
   SelectItem,
 } from "@/components/ui/select";
-
 import { FaPhoneAlt, FaEnvelope } from "react-icons/fa";
 import { motion } from "framer-motion";
 import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import emailjs from "emailjs-com";
 
 const info = [
   {
@@ -31,67 +34,109 @@ const info = [
 ];
 
 const Contact = () => {
-  const [formData, setFormData] = useState({
-    firstname: "",
-    lastname: "",
-    email: "",
-    phone: "",
-    service: "",
+  const validationSchema = Yup.object({
+    firstName: Yup.string().required("Firstname is required"),
+    lastName: Yup.string().required("Lastname is required"),
+    email: Yup.string().email().required("Email is required"),
+    phone: Yup.string().required("Phone is required"),
+    service: Yup.string().required("Service is required"),
+    message: Yup.string().required("Message is required"),
   });
 
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      phone: "",
+      service: "",
+      message: "",
+      honeypot: "",
+    },
+    validationSchema,
+    onSubmit: async (values, { resetForm }) => {
+      if (values.honeypot) {
+        alert("Bot detected");
+        return;
+      }
 
-  const handleSelectChange = (value) => {
-    setFormData({ ...formData, service: value });
-  };
+      try {
+        console.log("Form values:", values);
+
+        await emailjs.send(
+          "service_euq5kho",
+          "template_cf8p3h4",
+          values,
+          "w7HRjsIie8woopJIN",
+        );
+        alert("Message sent successfully!");
+        resetForm();
+      } catch (error) {
+        console.error("Error sending message:", error);
+        alert("Failed to send message. Please try again.");
+      }
+    },
+  });
 
   return (
     <motion.section
       initial={{ opacity: 0 }}
-      animate={{
-        opacity: 1,
-      }}
+      animate={{ opacity: 1 }}
       className="py-6"
     >
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row gap-[30px]">
-          {/* form */}
           <div className="xl:h-[54%] order-2 xl:order-non">
-            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+            <form
+              onSubmit={formik.handleSubmit}
+              className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl"
+            >
               <h3 className="text-4xl text-yellow-200">Get in touch</h3>
               <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
                 <Input
-                  name="firstname"
+                  name="firstName"
                   type="text"
                   placeholder="Firstname"
-                  value={formData.firstname}
-                  onChange={handleChange}
+                  {...formik.getFieldProps("firstName")}
                 />
+                {formik.touched.firstName && formik.errors.firstName ? (
+                  <div>{formik.errors.firstName}</div>
+                ) : null}
                 <Input
-                  name="lastname"
+                  name="lastName"
                   type="text"
                   placeholder="Lastname"
-                  value={formData.lastname}
-                  onChange={handleChange}
+                  {...formik.getFieldProps("lastName")}
                 />
+                {formik.touched.lastName && formik.errors.lastName ? (
+                  <div>{formik.errors.lastName}</div>
+                ) : null}
                 <Input
                   name="email"
                   type="email"
                   placeholder="Email address"
-                  value={formData.email}
-                  onChange={handleChange}
+                  {...formik.getFieldProps("email")}
                 />
+                {formik.touched.email && formik.errors.email ? (
+                  <div>{formik.errors.email}</div>
+                ) : null}
                 <Input
                   name="phone"
                   type="tel"
                   placeholder="Phone number"
-                  value={formData.phone}
-                  onChange={handleChange}
+                  {...formik.getFieldProps("phone")}
                 />
+                {formik.touched.phone && formik.errors.phone ? (
+                  <div>{formik.errors.phone}</div>
+                ) : null}
               </div>
-              <Select onValueChange={handleSelectChange}>
+              <Select
+                name="service"
+                value={formik.values.service}
+                onValueChange={(value) =>
+                  formik.setFieldValue("service", value)
+                }
+              >
                 <SelectTrigger className="w-full">
                   <SelectValue placeholder="Select a service" />
                 </SelectTrigger>
@@ -105,19 +150,33 @@ const Contact = () => {
                   </SelectGroup>
                 </SelectContent>
               </Select>
+              {formik.touched.service && formik.errors.service ? (
+                <div>{formik.errors.service}</div>
+              ) : null}
+
               <Textarea
                 className="h-[200px]"
                 placeholder="Type your message here."
                 name="message"
-                value={formData.message}
-                onChange={handleChange}
+                {...formik.getFieldProps("message")}
               />
-              <Button size="md" className="max-w-40">
+              {formik.touched.message && formik.errors.message ? (
+                <div>{formik.errors.message}</div>
+              ) : null}
+              <div style={{ display: "none" }}>
+                <label htmlFor="honeypot">Honeypot</label>
+                <input
+                  id="honeypot"
+                  name="honeypot"
+                  type="text"
+                  {...formik.getFieldProps("honeypot")}
+                />
+              </div>
+              <Button type="submit" size="md" className="max-w-40">
                 Send message
               </Button>
             </form>
           </div>
-          {/* info */}
           <div className="flex-1 items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
             <ul className="flex flex-col gap-8">
               {info.map((item, index) => (
